5dcc7a0b344412e5fbb34ee448c401271c2cb0cb
mikaelssen <mikael@rubixy.com>
2018-02-24 13:57:32 +0100

    hacked inn some buggy view moment

--- Space/SFMLWINDOW.cs
+++ Space/SFMLWINDOW.cs
@@ -2,6 +2,7 @@
 using System;
 using SFML.Graphics;
 using SFML.System;
+using SFML.Window;
 
 class SFMLWindow
 {
@@ -9,16 +10,22 @@ class SFMLWindow
 	public DrawingSurface rendersurface;
 	RenderWindow renderwindow;
 	System.Windows.Forms.Form form;
+	View view;
+
 	public SFMLWindow()
 	{
 		// initialize the form
 		form = new System.Windows.Forms.Form // create our form
 		{
-			Size = new System.Drawing.Size(800, 800), // set form size to 600 width & 600 height
+			Size = new System.Drawing.Size(800, 800), 
 			AutoSize = true,
 		};
-		form.Show(); // show our form
+		form.Show(); 
+
+		//event listeners
 		form.Resize += Form_Resize;
+		form.KeyPress += Form_KeyPress;
+
 		rendersurface = new DrawingSurface// our control for SFML to draw on
 		{
 			Size = form.Size, // set our SFML surface control size to be 500 width & 500 height
@@ -27,7 +34,11 @@ class SFMLWindow
 
 		// initialize sfml
 		renderwindow = new RenderWindow(rendersurface.Handle); // creates our SFML RenderWindow on our surface control
-
+		//set view
+		view = renderwindow.GetView();
+		//event handler for keys
+		renderwindow.KeyPressed += Renderwindow_KeyPressed;
+		//TODO make sure that the window is selected properly, as it won't allow for movement if it's not. BUG
 
 		// drawing loop
 		while (form.Visible) // loop while the window is open
@@ -35,18 +46,48 @@ class SFMLWindow
 			System.Windows.Forms.Application.DoEvents(); // handle form events
 			renderwindow.DispatchEvents(); // handle SFML events - NOTE this is still required when SFML is hosted in another window
 			renderwindow.Clear(Color.Black); // clear our SFML RenderWindow
+
 			Draw();
 			renderwindow.Display(); // display what SFML has drawn to the screen
 		}
 	}
 
+	private void Form_KeyPress(object sender, System.Windows.Forms.KeyPressEventArgs e)
+	{
+			//handle sfml input here i guess?
+	}
+
 	private void Form_Resize(object sender, EventArgs e)
 	{
 		rendersurface.Size = form.Size;
+		if (renderwindow != null)
+		{
+			renderwindow.Size = new Vector2u((uint)form.Size.Width, (uint)form.Size.Height);
+		}
+	}
+
+	private void Renderwindow_KeyPressed(object sender, SFML.Window.KeyEventArgs e)
+	{
+		int speed = 10;
+		//move the view
+		if (e.Code == Keyboard.Key.A)
+			view.Move(new Vector2f(-speed,  0));
+		if (e.Code == Keyboard.Key.D)
+			view.Move(new Vector2f( speed,  0));
+		if (e.Code == Keyboard.Key.W)
+			view.Move(new Vector2f( 0, -speed));
+		if (e.Code == Keyboard.Key.S)
+			view.Move(new Vector2f( 0,  speed));
+
+		//System.Diagnostics.Debug.WriteLine(e.Code.ToString());
+
 	}
 
 	public void Draw()
 	{
+
+		//set view
+		renderwindow.SetView(view);
 		//TODO Only render the one active system 
 		if (Game.Systems.Count <= 0)
 		{
